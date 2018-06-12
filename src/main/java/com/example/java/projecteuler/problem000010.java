6ce977011a0d17a8b6c2da83c730ae5b5a7c161f
huc003 <459382234@qq.com>
2018-06-12 14:31:33 +0800

    编程题

--- src/main/java/com/example/java/projecteuler/problem000010.java
+++ src/main/java/com/example/java/projecteuler/problem000010.java
@@ -1,5 +1,7 @@
 package com.example.java.projecteuler;
 
+import java.util.BitSet;
+
 /**
  * @Author: 胡成
  * @Version: 0.0.1V
@@ -11,21 +13,57 @@ package com.example.java.projecteuler;
  **/
 public class problem000010 {
     public static void main(String[] args) {
-        getResult();
+        getResult(2000000);
+        getPrimeNumberSum(2000000);
     }
 
-    public static void getResult() {
-        int j,sum=0;
-        for (int i = 2; i < 2000000; i++) {
-            j = 2;
-            while (i % j != 0) {
-                j++;
-            }
-            if(j==i){
-                System.out.println(i);
-                sum+=i;
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        double max = Math.sqrt(n);
+        for (int i = 2; i <= max; i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
-        System.out.println(sum);
+        return true;
+    }
+
+    private static void getPrimeNumberSum(int n) {
+        long start = System.currentTimeMillis();
+        int i = 5;
+        //由于2，3都是质数，初始值为5
+        Long sum = 5L;
+        while (i <= n) {
+            //质数 不能被2整除
+            if (isPrimeNumber(i += 2)) {
+                sum += i;
+            }
+            //不能被3整除
+            if (i <= n && isPrimeNumber(i += 4)) {
+                sum += i;
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("sum的值:"+sum);
+        System.out.println("The time cost is " + (end - start));
+    }
+
+    private static void getResult(int n){
+        long start = System.currentTimeMillis();
+        long sum = 0;
+        l:for(int i = 2; i < n; i ++) {
+            //当j小于根号i的时候，继续循环。
+            for(int j = 2; j < Math.sqrt(i); j ++){
+                if(i % j == 0){
+                    continue l;
+                }
+            }
+            sum+=i;
+        }
+        System.out.println("sum的值:"+sum);
+        long end = System.currentTimeMillis();
+        System.out.println("The time cost is " + (end - start));
     }
 }
